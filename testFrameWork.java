8765561810d3b65dfb4dfa17bddd990bddc256f3
typsadde <shemani@kth.se>
2016-03-04 13:35:28 +0100

    read from file
    read from file done

--- testFrameWork.java
+++ testFrameWork.java
@@ -1,20 +1,70 @@
 import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.io.FileInputStream;
 
 public class testFrameWork {
 
 	public static void main(String[] args) {
-		
+		String[] combArray = new String[194];
+		Path filePath = Paths.get("C:/Users/sayye/Downloads","jenny_output.txt");
+		int k = 0;
+		int n;
+		byte bufferByte;
+		FileInputStream fis = null;
+
+		try{
+			fis = new FileInputStream("C:/Users/sayye/Downloads/jenny_output.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis)); 
+			String line;
+			while(((line=br.readLine())!=null) || k<194) {
+			//	System.out.println(line);
+				combArray[k] = line;
+				k++;
+			} 
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		for (int i=0;i<194;i++) {
+			System.out.println(i);
+		}
+		System.out.println(Arrays.toString(combArray));
+
+		/*try(InputStream in = Files.newInputStream(filePath)) {
+			while ((n = in.read()) != -1) {
+				if(n == 0) {
+					break;
+				}
+				if((char)n != ' ') {
+				//	System.out.println((char)n);
+					combArray[k] = (char)n;
+				//	System.out.println(combArray[k]);
+				}
+			}
+			System.out.println(Arrays.toString(combArray));
+		} catch (IOException e) {
+			System.err.println(e);
+		}*/
 		sort sortStuff = new sort();
 		Random rand = new Random();
 		int[] array = new int[20];
-		
+		double[] arrayLong = new double[20];
 		for (int i=0;i<array.length-1;i++) {
 			array[i] = rand.nextInt(100);
 		}
-		sortStuff.sortArray(array);
-				//System.out.println(Arrays.toString(array));
+		/*for(double j=0.1;j<arrayLong.length-1;j++) {
+			arrayLong[j] = rand.nextInt(100);
+		}*/
+		sortStuff.sortArray(array);	
+		//System.out.println(Arrays.toString(array));
 		int randommember = rand.nextInt(100);
-		
 		System.out.println("Looking for member: " + randommember);
 
 		int memberIndex = sortStuff.membQuery(array, randommember);
@@ -27,3 +77,4 @@ public class testFrameWork {
 	}
 
 }
+
